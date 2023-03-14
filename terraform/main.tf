69110720ca993a3bd6d94de32a077142b0389281
NTAP3 <122728522+NTAP3@users.noreply.github.com>
2023-03-14 09:16:23 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "this" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
-  security_groups = var.security_group_ids
+  vpc_security_group_ids = var.security_group_ids
   subnet_id     = var.subnet_id
   associate_public_ip_address = true
   user_data     = base64encode(local.user_data)
